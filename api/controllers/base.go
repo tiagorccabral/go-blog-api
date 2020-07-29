0f64ca004f8cd3bfdbdeb9546448524f6a1df12c
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-07-29 16:35:17 -0300

    :card_file_box: Adds DB configs on server initialization
    Using GORM ORM to initialize DB using PGsql. Fetches DB credentials from .env file

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -8,16 +8,39 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/tiagorccabral/go-blog-api/api/models"
+
+	//postgres database driver
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // Server base type. Contains pointer to router
 type Server struct {
+	DB     *gorm.DB
 	Router *mux.Router
 }
 
 // Initialize server router and route handlers
-func (server *Server) Initialize() {
+func (server *Server) Initialize(DBDriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
 
+	var err error
+
+	if DBDriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(DBDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Failed to connect to %s database\n", DBDriver)
+			log.Fatal("The following error happened: ", err)
+		} else {
+			log.Printf("Connected to %s database. DB name: %s\n", DBDriver, DbName)
+		}
+	}
+
+	// Generates DB mirations
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{})
+
+	// Initializes Router
 	server.Router = mux.NewRouter()
 
 	server.initializeRoutes()
